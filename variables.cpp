5af307bf002ffe404dbbabfd8fbf13c3601d3536
KyawwZaya <97281705+KyawwZaya@users.noreply.github.com>
2022-03-04 12:16:52 +0630

    variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    string characterName = "John";
+    int characterAge ;
+    characterAge = 35;
+    cout << "There once was a man named " << characterName << '\n';
+    cout << "He was " << characterAge << " years old" << '\n';
+    characterName = "Mike";
+
+    cout << "He like the name " << characterName << '\n';
+    cout << "But he not like being " << characterAge << '\n';
+    
+
+    return 0;
+}
